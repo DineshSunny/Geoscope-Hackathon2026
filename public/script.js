297351ad35dd06cbf64f731add76bc9abbb3382d
DINESH SUNNY <57325284+DineshSunny@users.noreply.github.com>
2026-03-29 00:28:38 +0000

    update

---
+++ public/script.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHBuDVBkzxDjT1A1LUpmj6sGumu3VYI6w",
+  authDomain: "geoscope-4d307.firebaseapp.com",
+  projectId: "geoscope-4d307",
+  storageBucket: "geoscope-4d307.firebasestorage.app",
+  messagingSenderId: "339709550664",
+  appId: "1:339709550664:web:f82c7fb2a03f3e5c88787d",
+  measurementId: "G-Q9S0HJ8DQK"
+};
